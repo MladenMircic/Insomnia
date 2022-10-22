e3a39e57f472b270a2cf4b2d73889206a31fbbd5
VukAmidzic <vuk.amidzic@gmail.com>
2022-10-22 16:39:56 +0200

    tree objects added

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,14 +6,19 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer groundSprite;
     [SerializeField] private int breadcrumbCount = 10;
+    private int treeCount;
     [SerializeField] private Sprite circle;
     private float breadcrumbOffsetY;
+    [SerializeField] private Sprite treeSprite;
+    [SerializeField] private GameObject treePrefab;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        generateBreadcrumbs();
+        treeCount = 500;
+        generateSafeZones();
+        generateTrees();
     }
 
     // Update is called once per frame
@@ -22,7 +27,7 @@ public class GameManager : MonoBehaviour
         
     }
 
-    private void generateBreadcrumbs()
+    private void generateSafeZones()
     {
         breadcrumbOffsetY = groundSprite.transform.position.y -
             groundSprite.bounds.size.y / 2;
@@ -38,6 +43,40 @@ public class GameManager : MonoBehaviour
                 new Vector3(randomOffsetX, breadcrumbOffsetY),
                 Quaternion.identity);
             breadcrumbOffsetY += offset;
+        }
+    }
+
+    private void generateTrees()
+    {
+        float mapWidth = groundSprite.bounds.size.x / 2;
+        float mapHeight = groundSprite.bounds.size.y / 2;
+        for (int i = 0; i < treeCount; i++)
+        {
+            var tree = Instantiate(treePrefab);
+            var quadrant = i % 4;
+            Debug.Log(quadrant);
+            float randomOffsetX = 0.0f, randomOffsetY = 0.0f;
+            switch (quadrant) {
+                case 0:
+                    randomOffsetX = Random.Range(0, mapWidth);
+                    randomOffsetY = Random.Range(0, mapHeight);
+                    break;
+                case 1:
+                    randomOffsetX = Random.Range(-mapWidth, 0);
+                    randomOffsetY = Random.Range(0, mapHeight);
+                    break;
+                case 2:
+                    randomOffsetX = Random.Range(-mapWidth, 0);
+                    randomOffsetY = Random.Range(-mapHeight, 0);
+                    break;
+                case 3:
+                    randomOffsetX = Random.Range(0, mapWidth);
+                    randomOffsetY = Random.Range(-mapHeight, 0);
+                    break; 
+            }
+            tree.transform.SetPositionAndRotation(
+                new Vector3(randomOffsetX, randomOffsetY),
+                Quaternion.identity);
         }
     }
 }
