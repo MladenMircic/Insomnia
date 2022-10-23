3f14dccdf18dab8c21be6707ae72005faeba1b8c
VukAmidzic <vuk.amidzic@gmail.com>
2022-10-23 12:31:53 +0200

    HP bar

--- Assets/ParticlesMovement.cs
+++ Assets/ParticlesMovement.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class ParticlesMovement : MonoBehaviour
 {
     public ParticleSystem particleSystem;
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
     private float h, v;
+    private float secondsSinceLastInt;
+    [SerializeField] private float intervalInSeconds = 1.0f;
     void Start()
     {
         h = 0.0f;
         v = 0.0f;
+        currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
     }
 
     
@@ -19,6 +28,13 @@ public class ParticlesMovement : MonoBehaviour
         v = Input.GetAxisRaw("Vertical");
 
         this.transform.Translate(h * 5.0f * Time.deltaTime, v * 5.0f * Time.deltaTime, 0.0f);
+
+        secondsSinceLastInt += Time.deltaTime;
+        if (secondsSinceLastInt >= intervalInSeconds)
+		{
+			TakeDamage(3);
+            secondsSinceLastInt = 0.0f;
+		}
 
         if (v != 0.0f || h != 0.0f) 
         {
@@ -45,4 +61,11 @@ public class ParticlesMovement : MonoBehaviour
             particleSystem.Stop();
         }
     }
+
+    void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+
+		healthBar.SetHealth(currentHealth);
+	}
 }
